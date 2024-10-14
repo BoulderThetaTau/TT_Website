50440548eb9f783327d287361b1c35f469b8e998
Liam Masias <liammasias2@gmail.com>
2024-10-13 20:19:18 -0600

    Adding Eta Gamma Logo and CSS properties

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -28,8 +28,8 @@ function Navbar() {
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-              <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                TRVL <i className='fab fa-typo3' />
+              <Link to="/" className="navbar-logo" onClick={closeMobileMenu} >
+                <i className='logo' />  ETA GAMMA
               </Link>
               <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />

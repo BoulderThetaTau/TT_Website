c5408d10cea845bc12abb447e40e5a08e034fbcd
LiamMasias <129802945+LiamMasias@users.noreply.github.com>
2024-10-13 19:46:57 -0600

    Uploading Skeleton Made From Tutotial
    Made skeleton from a React Tutorial by Brian Design.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './Components/pages/Home';
+import Services from './Components/pages/Services';
+import Products from './Components/pages/Products';
+import SignUp from './Components/pages/SignUp';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact Component={Home}/>
+        <Route path='/services' Component={Services} />
+        <Route path='/products' Component={Products} />
+        <Route path='/sign-up' Component={SignUp} />
+      </Routes>
+      <Footer />
+    </Router>
+      
+    </>
+  );
+}
+
+export default App;

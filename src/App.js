85ed63c1ac2f9fa4154baa8bedf77abcd7a777ac
Liam Masias <liammasias2@gmail.com>
2024-10-16 18:21:33 -0600

    Removing old skelton pages, making pillar pages permanent

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Components/pages/Home';
-import Services from './Components/pages/Services';
-import Products from './Components/pages/Products';
 import SignUp from './Components/pages/SignUp';
 import Footer from './Components/Footer';
 import Brotherhood from './Components/pages/Brotherhood';
@@ -18,8 +16,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact Component={Home}/>
-        <Route path='/services' Component={Services} />
-        <Route path='/products' Component={Products} />
         <Route path='/brotherhood' Component={Brotherhood} />
         <Route path='/comm-serve' Component={CommServ} />
         <Route path='prof-def' Component={ProfDef} />

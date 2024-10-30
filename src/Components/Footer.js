d846e3adec57bb4a4ac635d3d885fc5987f6fc7b
Liam Masias <liammasias2@gmail.com>
2024-10-29 20:43:43 -0600

    Removing skeleton elements and adding Theta Tau Logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,27 +6,12 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section>
       <div className='footer-links'>
         <div className='footer-link-wrapper'>
+          <div className='footer-link-items footer-logo'>
+            <img src='../public/images/ThetaTauLogo.png'>
+            </img>
+          </div>
             <div className='footer-link-items'>
                 <h2>About Us</h2>
                 <Link to='sign-up'>How it Works</Link>
@@ -62,11 +47,6 @@ function Footer() {
       </div>
       <section className="social-media">
         <div className="social-media-wrap">
-            <div className="footer-logo">
-                <Link to='/' className="social-logo">
-                    TRVL <i className="fab fa-typo3"></i>
-                </Link>
-            </div>
             <small className="website-rights">TRVL © 2024</small>
             <div className="social-icons">
                 <Link className="social-icon-link facebook"

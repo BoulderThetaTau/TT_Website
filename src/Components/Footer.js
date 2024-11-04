335fc7675ff8b54c90ae04b0750ec2cd0a024b2b
Liam Masias <liammasias2@gmail.com>
2024-11-04 14:12:45 -0700

    Added styling to footer items and reduced skeleton footprint

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,47 +7,37 @@ function Footer() {
   return (
     <div className='footer-container'>
       <div className='footer-links'>
-        <div className='footer-link-wrapper'>
+        <div className='footer-link-wrapper' style={{gap: '5px'}}>
           <div className='footer-link-items footer-logo'>
-            <img src='../public/images/ThetaTauLogo.png'>
-            </img>
+            <img src='../public/images/ThetaTauLogo.png'></img>
           </div>
             <div className='footer-link-items'>
                 <h2>About Us</h2>
-                <Link to='sign-up'>How it Works</Link>
-                <Link to='/'>Testimonials</Link>
-                <Link to='/'>Careers</Link>
-                <Link to='/'>Investors</Link>
-                <Link to='/'>Terms of Service</Link>
+                <Link to='/'>How it Works</Link>
+                <Link to='/'>Mercury Council</Link>
+                <Link to='/'>Alumni</Link>
+                <Link to='/'>Our Three Pillars</Link>
             </div>
-            <div className='footer-link-items'>
-                <h2>Contact Us</h2>
-                    <Link to='/'>Contact</Link>
-                    <Link to='/'>Support</Link>
-                    <Link to='/'>Destinations</Link>
-                    <Link to='/'>Sponsorships</Link>
-            </div>
-        </div>
-        <div className='footer-link-wrapper'>
-            <div className='footer-link-items'>
-                <h2>Videos</h2>
-                    <Link to='/'>Submit Video</Link>
-                    <Link to='/'>Ambassadors</Link>
-                    <Link to='/'>Agency</Link>
-                    <Link to='/'>Influencer</Link>
-            </div>
-            <div className='footer-link-items'>
+            {/* Once we get more social media platforms, we can uncomment this out 
+              <div className='footer-link-items'>
                 <h2>Social Media</h2>
-                    <Link to='/'>Instagram</Link>
+                    <Link to='https://www.instagram.com/boulderthetatau?igsh=eGpweWNub3dpc2I0' target='_blank'>Instagram</Link>
                     <Link to='/'>Facebook</Link>
                     <Link to='/'>Youtube</Link>
                     <Link to='/'>Twitter</Link>
+            </div> */}
+            <div className='footer-link-items'>
+                <h2>Contact Us</h2>
+                    <Link to='/'>Contact</Link>
+                    <Link to='https://www.instagram.com/boulderthetatau?igsh=eGpweWNub3dpc2I0' target='_blank'>Instagram</Link>
+                    <Link to='https://thetatau.org/' target='_blank'>National Theta Tau</Link>
+                    <Link to='mailto:central.office@thetatau.org' className='email-link'>central.office@thetatau.org</Link>
             </div>
         </div>
       </div>
       <section className="social-media">
         <div className="social-media-wrap">
-            <small className="website-rights">TRVL © 2024</small>
+            <small className="website-rights">THETA TAU 2024</small>
             <div className="social-icons">
                 <Link className="social-icon-link facebook"
                 to='/'
@@ -57,7 +47,7 @@ function Footer() {
                     <i className="fab fa-facebook-f"></i>
                 </Link>
                 <Link className="social-icon-link instagram"
-                to='/'
+                to='https://www.instagram.com/boulderthetatau?igsh=eGpweWNub3dpc2I0'
                 target='_blank'
                 aria-label='Instagram'
                 >

5283c279ee5ae0948b42528d36fa6574bcceb8d3
Liam Masias <liammasias2@gmail.com>
2024-11-04 18:45:35 -0700

    Put up video gor background instead, for now

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
         {/* if you don't want the video, comment out the video tag below */}
-      {/* <video src='/videos/video-2.mp4' autoPlay loop muted />  */}
+      <video src='/videos/video-1.mp4' autoPlay loop muted /> 
       <h1>THETA TAU ETA GAMMA CHAPTER</h1>
       <p>The Engineering Fraternity at the University of Colorado - Boulder</p>
       <div className='hero-btns'>

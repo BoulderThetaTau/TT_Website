ec0bcfc4de1395386c68cc72fedd6e571ff3c24f
Liam Masias <liammasias2@gmail.com>
2024-11-04 19:23:58 -0700

    Adjusted background and text of homepage

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -2,14 +2,15 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import { Link } from 'react-router-dom';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
         {/* if you don't want the video, comment out the video tag below */}
-      <video src='/videos/video-1.mp4' autoPlay loop muted /> 
+      {/* <video src='/videos/video-1.mp4' autoPlay loop muted />  */}
       <h1>THETA TAU ETA GAMMA CHAPTER</h1>
-      <p>The Engineering Fraternity at the University of Colorado - Boulder</p>
+      <p>The Co-Ed Professional Engineering Fraternity at the University of Colorado - Boulder</p>
       <div className='hero-btns'>
         <Button className='btns'
         buttonStyle='btn--outline'
@@ -21,7 +22,7 @@ function HeroSection() {
         buttonStyle='btn--primary'
         buttonSize='btn--large'
         >
-            WATCH TRAILER <i className='far fa-play-circle' />
+          <Link to='https://thetatau.org' style={{textDecoration: 'none', color: 'black'}}>NATIONALS</Link>
         </Button>
       </div>
     </div>

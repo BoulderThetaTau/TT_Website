c0568288b5c5498f996e7e4b4dbc4df2bb5d1725
Liam Masias <liammasias2@gmail.com>
2024-11-24 02:12:44 -0700

    adding theta tau colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        thetaTauGold: '#CFB87C', // Example hex value for gold
+        thetaTauRed: '#8B0000', // Example hex value for dark red
+      },
+    },
   },
   plugins: [],
 }

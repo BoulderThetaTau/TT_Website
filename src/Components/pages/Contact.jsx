0c97d03bf44dcb1a2422b60e47db161ab7f575d8
Liam Masias <liammasias2@gmail.com>
2025-01-18 21:30:05 -0700

    Added layout for contact page
    Still need to add photos and acutal details. Let's also find a way to add some flaire to this page when possible

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -8,6 +8,29 @@ function Contact(){
             <div className='ContactHead'>
                 <h1>Contact Us</h1>
             </div>
+            <div className="ContactBody">
+                <div className="ContactInfo columns-2 grid">
+                    <div className="EtaGammaContact grid-flow-row">
+                        <h2>Eta Gamma Chapter</h2>
+                        <div className="columns-1">
+                            <p>email:</p>
+                            <p>School Address:</p>
+                        </div>
+                    </div>
+                    <div className="NationalsContact grid-flow-row">
+                        <h2>Nationals</h2>
+                        <div className="columns-1">
+                            <p>Phone:</p>
+                            <p>email:</p>
+                            <p>Mailing Address:</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="ContactPhotos">
+
+                </div>
+            </div>
         </div>
     )
 }

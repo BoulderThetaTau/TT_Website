f24e73c4a92172d6a4c8d32e6a64f7bf14a3a563
Liam Masias <liammasias2@gmail.com>
2025-01-18 20:48:14 -0700

    Added contact page to app, also added head piece for page

---
+++ src/Components/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import '../../App.css';
+import './pagesCSS/Contact.css';
+
+function Contact(){
+    return(
+        <div className="ContactPage">
+            <div className='ContactHead'>
+                <h1>Contact Us</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Contact

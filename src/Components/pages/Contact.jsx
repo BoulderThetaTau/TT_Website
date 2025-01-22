d98583fa2cd40345aef8b281c895e4cf70c87356
Liam Masias <liammasias2@gmail.com>
2025-01-21 20:28:15 -0700

    Mostly completed content for contact page
    Need to update mobile formatting and figure out how to get address in there for nationals

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -9,29 +9,34 @@ function Contact(){
                 <h1>Contact Us</h1>
             </div>
             <div className="ContactBody">
-                <div className="ContactInfo columns-2 grid">
+                <div className="ContactInfo columns-2">
                     <h1>Eta Gamma Chapter</h1>
-                    <div className="EtaGammaContact grid-flow-row">
-                        <div className="columns-1">
-                            <p>email:</p>
-                            <p>School Address:</p>
-                        </div>
+                    <div className="EtaGammaEmail columns-2" style={{marginTop: "1rem"}}>
+                        <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                        <p>regent@etagamma.org</p>
                     </div>
-                <h1>Nationals</h1>
-                    <div className="NationalsContact grid">
-                        <div className="NatContactInfo columns-2 grid-flow-col">
-                            <i class="fa fa-phone fa-2x bg-thetaTauRed" aria-hidden="true"></i><br />
-                            <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
-                            <p>(512) 472-1904</p>
-                            <p>central.office@thetatau.org</p>
-                        </div>
+                    <div className="EtaGammaAddress columns-2" style={{marginTop: "1rem"}}>
+                        <i class="fa fa-map-marker fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                        <p>1111 Engineering Dr, <br/>
+                        Boulder, CO 80309</p>
+                    </div>
+                    <h1>Nationals</h1>
+                    <div className="NationalsPhone columns-2" style={{marginTop: "1rem"}}>
+                        <i class="fa fa-phone fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                        <p>(512) 472-1904</p>
+                    </div>
+                    <div className="NationalsEmail columns-2" style={{marginTop: "1rem"}}>
+                        <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                        <p>central.office@thetatau.org</p>
+                    </div>
+                    <div className="EtaGammaImage">
+                        <img src="images/EtaGamma_Contact.png" alt="Nationals"/>
+                    </div>
+                    {/* <div className="NationalsMailAdd">
                         <h2 className="text-thetaTauRed">Mailing Address:</h2>
                         <p>2131 W. Republic Road #528 <br />
                         Springfield, MO 65807</p>
-                    </div>
-                </div>
-                <div className="ContactPhotos">
-
+                    </div> */}
                 </div>
             </div>
         </div>

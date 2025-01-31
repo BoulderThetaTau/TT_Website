00042bce0c247a7398ec09a3959263a3fb0f4bdd
Liam Masias <liammasias2@gmail.com>
2025-01-30 20:21:50 -0700

    Updating some styling and formatting to the contact page

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -9,34 +9,36 @@ function Contact(){
                 <h1>Contact Us</h1>
             </div>
             <div className="ContactBody">
-                <div className="ContactInfo columns-2">
-                    <h1>Eta Gamma Chapter</h1>
-                    <div className="EtaGammaEmail columns-2" style={{marginTop: "1rem"}}>
-                        <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
-                        <p>regent@etagamma.org</p>
+                <div className="MainContact columns-1 mobile:columns-2">
+                    <div className="ContactInfo columns-1">
+                        <h1>Eta Gamma Chapter</h1>
+                        <div className="EtaGammaEmail columns-2" style={{marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C"}}>
+                            <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                            <p>regent@etagamma.org</p>
+                        </div>
+                        <div className="EtaGammaAddress columns-2" style={{marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C"}}>
+                            <i class="fa fa-map-marker fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                            <p>1111 Engineering Dr,
+                            Boulder, CO 80309</p>
+                        </div>
+                        <h1>Nationals</h1>
+                        <div className="NationalsPhone columns-2" style={{marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C"}}>
+                            <i class="fa fa-phone fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                            <p>(512) 472-1904</p>
+                        </div>
+                        <div className="NationalsEmail columns-2" style={{marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C"}}>
+                            <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                            <p>central.office@thetatau.org</p>
+                        </div>
+                        <div className="NationalsMailAdd" style={{marginTop: "1rem"}}>
+                            <h2 className="text-thetaTauRed">Mailing Address:</h2>
+                            <p>2131 W. Republic Road #528 <br />
+                            Springfield, MO 65807</p>
+                        </div>
                     </div>
-                    <div className="EtaGammaAddress columns-2" style={{marginTop: "1rem"}}>
-                        <i class="fa fa-map-marker fa-2x bg-thetaTauRed" aria-hidden="true"></i>
-                        <p>1111 Engineering Dr, <br/>
-                        Boulder, CO 80309</p>
-                    </div>
-                    <h1>Nationals</h1>
-                    <div className="NationalsPhone columns-2" style={{marginTop: "1rem"}}>
-                        <i class="fa fa-phone fa-2x bg-thetaTauRed" aria-hidden="true"></i>
-                        <p>(512) 472-1904</p>
-                    </div>
-                    <div className="NationalsEmail columns-2" style={{marginTop: "1rem"}}>
-                        <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
-                        <p>central.office@thetatau.org</p>
-                    </div>
-                    <div className="EtaGammaImage">
+                    <div className="EtaGammaImage mobile:justify-center" style={{display: "flex"}}>
                         <img src="images/EtaGamma_Contact.png" alt="Nationals"/>
                     </div>
-                    {/* <div className="NationalsMailAdd">
-                        <h2 className="text-thetaTauRed">Mailing Address:</h2>
-                        <p>2131 W. Republic Road #528 <br />
-                        Springfield, MO 65807</p>
-                    </div> */}
                 </div>
             </div>
         </div>

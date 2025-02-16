c3953702e4cc8e5fcf5858c1e4d5a1c27c58b606
Liam Masias <liammasias2@gmail.com>
2025-02-15 18:17:23 -0700

    Fixing up structure of contacts page

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -9,7 +9,7 @@ function Contact(){
                 <h1>Contact Us</h1>
             </div>
             <div className="ContactBody">
-                <div className="MainContact columns-1 mobile:columns-2">
+                <div className="MainContact columns-1">
                     <div className="ContactInfo columns-1">
                         <h1>Eta Gamma Chapter</h1>
                         <div className="EtaGammaEmail columns-2" style={{marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C"}}>
@@ -30,13 +30,13 @@ function Contact(){
                             <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
                             <p>central.office@thetatau.org</p>
                         </div>
-                        <div className="NationalsMailAdd" style={{marginTop: "1rem"}}>
+                        <div className="NationalsMailAdd columns-2" style={{marginTop: "1rem"}}>
                             <h2 className="text-thetaTauRed">Mailing Address:</h2>
                             <p>2131 W. Republic Road #528 <br />
                             Springfield, MO 65807</p>
                         </div>
                     </div>
-                    <div className="EtaGammaImage mobile:justify-center" style={{display: "flex"}}>
+                    <div className="EtaGammaImage justify-center" style={{display: "flex"}}>
                         <img src="images/EtaGamma_Contact.png" alt="Nationals"/>
                     </div>
                 </div>

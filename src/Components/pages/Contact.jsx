de7bb1bfec1aec1b1e2d37e75a9288aa411c631f
LiamMasias <liam.masias@colorado.edu>
2025-03-31 23:31:07 -0600

    Fixed contact page bug with shrinking icons

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -14,24 +14,28 @@ function Contact(){
                     <div className="ContactInfo columns-1">
                         <h1 className="font-semibold">Eta Gamma Chapter</h1>
                         <div className="EtaGammaEmail columns-2 flex items-center gap-x-4" style={{ marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C" }}>
-                            <i className="fa fa-envelope fa-2x bg-thetaTauRed flex items-center justify-center" aria-hidden="true"></i>
+                            <i className="fa fa-envelope fa-2x bg-thetaTauRed rounded-full flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14" 
+                            aria-hidden="true"></i>
                             <p className="whitespace-nowrap">
                                 <a href="mailto:regent@etagamma.org">regent@etagamma.org</a>
                             </p>
                         </div>
                         <div className="EtaGammaAddress columns-2 flex items-center gap-x-4" style={{marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C"}}>
-                            <i class="fa fa-map-marker fa-2x bg-thetaTauRed flex items-center justify-center" aria-hidden="true"></i>
+                            <i class="fa fa-map-marker fa-2x bg-thetaTauRed rounded-full flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14 aspect-square shrink-0" 
+                            aria-hidden="true"></i>
                             <p className="whitespace-normal sm:whitespace-nowrap">
                             1111 Engineering Dr,
                             Boulder, CO 80309</p>
                         </div>
                         <h1 className="font-semibold">Nationals</h1>
                         <div className="NationalsPhone columns-2 flex items-center gap-x-4" style={{marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C"}}>
-                            <i class="fa fa-phone fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                            <i class="fa fa-phone fa-2x bg-thetaTauRed rounded-full flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14" 
+                            aria-hidden="true"></i>
                             <p className="whitespace-nowrap">(512) 472-1904</p>
                         </div>
                         <div className="NationalsEmail columns-2 flex items-center gap-x-4" style={{marginTop: "1rem", borderBottom: "solid", paddingBottom: ".5rem", borderColor: "#CFB87C"}}>
-                            <i class="fa fa-envelope fa-2x bg-thetaTauRed" aria-hidden="true"></i>
+                            <i class="fa fa-envelope fa-2x bg-thetaTauRed rounded-full flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14" 
+                            aria-hidden="true"></i>
                             <p className="whitespace-nowrap">
                                 <a href="mailto:central.office@thetatau.org">central.office@thetatau.org</a>
                             </p>

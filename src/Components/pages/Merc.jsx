2f30d131516725f79884419ad7b6e8f8614d017c
LiamMasias <liam.masias@colorado.edu>
2025-05-16 15:24:33 -0600

    Updated Regent and Vice Regent's Merc bios

--- src/Components/pages/Merc.jsx
+++ src/Components/pages/Merc.jsx
@@ -21,7 +21,7 @@ function Merc() {
       pledgeClass: "Eta Gamma 414 Y! TT! Omicron Gamma Pledge Class",
       image: "/images/Merc/Vice.png",
       description:
-        "Victoria is a sophomore majoring in Computer Science with a minor in Computer Engineering. Outside of school, she enjoys working as a learning assistant, exploring her hometown of Pittsburgh, PA, spending time outdoors, and trying new things. Her favorite part of Theta Tau is always knowing somebody in the engineering center.",
+        "Victoria is a sophomore majoring in Computer Science with a minor in Computer Engineering. Outside of school, she enjoys working as a learning assistant, exploring her hometown of Pittsburgh, PA, spending time outdoors, and trying new things. Her favorite part of Theta Tau is always knowing somebody in the engineering center. As Vice Regent, Victoria is excited to oversee event planning and help make others’ ideas come to life.",
     },
     {
       name: "Madison Petrucelli",

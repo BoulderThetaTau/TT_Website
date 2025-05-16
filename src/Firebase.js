3cb27f5af5acb8c66b7e1d839d06f4a9c8299feb
LiamMasias <liam.masias@colorado.edu>
2025-05-15 19:33:32 -0600

    Made the enviornment variable to safekeep the api keys

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDApp6qZG50gSx1jZj7ceh0AosxSvXNbk",
-  authDomain: "etagammawebsite.firebaseapp.com",
-  projectId: "etagammawebsite",
-  storageBucket: "etagammawebsite.firebasestorage.app",
-  messagingSenderId: "309290422114",
-  appId: "1:309290422114:web:9c432af43b48ffec93a63b",
-  measurementId: "G-67M6SFKNYW"
+  apiKey:               process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:           process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:            process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:        process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:                process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:        process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
